7a125a49f7034cfc36626a7f64078a325df90d85
RaevaaM <107184229+RaevaaM@users.noreply.github.com>
2023-01-20 10:58:43 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Jivotni.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Models/Poroda.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Jivotni.Models
+{
+	public class Poroda 
+	{
+	 
+	}
+}
